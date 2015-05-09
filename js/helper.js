2de75e7e2e6faf1a7c0d7dedc22cb4aa860b2b19
Michael Savin <my.savin@yandex.com>
2015-05-09 13:11:45 +0300

    Rename index.js to helper.js

--- js/helper.js
+++ js/helper.js
@@ -8,20 +8,14 @@ Array.prototype.repeat = function(value, count) {
 	}
 	return this;
 };
+
 Object.prototype.contains = function(obj) {
     for (var i in this)
         if (this[i] === obj)
             return true;
     return false;
-}
+};
+
 function d(id) {
 	return document.getElementById(id);
-}
-
-function win() {
-	alert("win");
-}
-
-function gameOver() {
-	alert("Game Over");
 }
